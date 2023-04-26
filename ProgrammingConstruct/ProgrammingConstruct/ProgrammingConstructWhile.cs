c7e2ec3752b82c6c65440308bc43c749ef33bcb4
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-04-26 13:27:32 +0530

    Added:Flip Coin

---
+++ ProgrammingConstruct/ProgrammingConstruct/ProgrammingConstructWhile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstruct
+{
+    public class ProgrammingConstructWhile
+    {
+        public void FlipCoin()
+        {
+            int headsCount = 0;
+            int tailsCount = 0;
+            Random random = new Random();
+            while (headsCount < 20 && tailsCount < 20)
+            {
+                int flip = random.Next(2);
+                if (flip == 0)
+                {
+                    headsCount++;
+                    Console.WriteLine("Heads");
+                }
+                else
+                {
+                    tailsCount++;
+                    Console.WriteLine("Tails");
+                }
+            }
+            if (headsCount == 20)
+            {
+                Console.WriteLine("Heads wins");
+            }
+            else
+            {
+                Console.WriteLine("Tails wins");
+            }
+        }
+    }
+}
